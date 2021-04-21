28cae74c137717c97764da725a044fca1bd51995
jaznyc <jaznycdev@gmail.com>
2021-04-21 18:30:48 -0400

    first angular code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
-    </div>
+   <div class='app'>
+   {{ title }}
+   <div>
+   {{numberOne + numberTwo}}
+   </div>
+   <div>
+   {{isHappy ? ':)' : ':('}}
+   </div>
+   </div>
   `
 })
 export class AppComponent {
+  numberOne: number = 1
+  numberTwo: number = 2
+  isHappy: boolean = true
+  title: string = 'Ultimate Angular'
 
 }
